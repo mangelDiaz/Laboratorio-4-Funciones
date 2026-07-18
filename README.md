b012a53c8a8774d4440370e79f7c42efa20e22bb
Miguel Ángel Díaz <mangeldiaz82@gmail.com>
2026-07-18 15:35:38 +0200

    Primer planteamiento pero con errores

--- README.md
+++ README.md
@@ -14,3 +14,11 @@
 3. Modificar estilos de root para que el hover y colores de los botones, se corresponda con los estilos nuevos.
 4. Jugar con las clases y los estilos para que los botones ocupen 1/3 del espacio, que el input ocupe el espacio restante y luego se reduzca en responsive
 5. Incorporar estilo active para los botones en root y eliminar focus
+6. Rehacer estilos llevandolo a azules
+
+## Añadir lógica en main.ts
+
+1. He repasado los pdfs de JS y TS y he copiado y pegado las partes más relevantes para las operaciones
+2. He añadido la parte de Pstart y he tratado de hacer la funcion de Sumando1 al pulsar el botón Siguiente
+3. Numerosos errores impedian que funcionara al estar en modo estricto. He ido informandome de cada error y mas o menos arreglandolo en la primera función de sumar +1 con el botón siguiente
+4. Con el mismo funcionamiento he sacado restar -1 para el botón Anterior, y ponerlo en 0 con el botón Reiniciar, sin embargo, no lo entendía muy bien y tenía diez errores de tipado, luego decidí parar y volver a ver las clases para repasar conceptos.
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,47 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+//Leer valor del html
+    const elementoMarcador = document.getElementById("marcador-turno");
+
+function sumandoUno () {
+    //Sumar 1 al valor
+    const resultadoSuma = parseInt(elementoMarcador.textContent) + 1;
+    // Mostrar resultado
+    document.getElementById("marcador-turno").innerHTML = resultadoSuma;
+}
+
+function restandoUno () {
+    //Restar 1 al valor
+    const resultadoResta = parseInt(elementoMarcador.textContent) - 1;
+    // Mostrar resultado
+    document.getElementById("marcador-turno").innerHTML = resultadoResta;
+}
+
+function reiniciarCero () {
+    //Reiniciar a cero
+    const resultadoReinicio = 0;
+    // Mostrar resultado
+    document.getElementById("marcador-turno").innerHTML = resultadoReinicio;
+
+}
+
+const botonSumar = document.getElementById("sumar");
+if (botonSumar) {
+botonSumar.addEventListener("click", sumandoUno);
+}
+
+const botonRestar = document.getElementById("restar");
+if (botonRestar) {
+botonRestar.addEventListener("click", restandoUno);
+}
+
+const botonReiniciar = document.getElementById("reiniciar");
+if (botonReiniciar) {
+botonReiniciar.addEventListener("click", reiniciarCero);
+}
+
+/*
+const marcador = "99";
+
+console.log(marcador.padStart(2, "0"));
+*/
